0b49eaf6e8cab96b8d854148b8b9bc3ff3a74b2e
alexmivonwien <alexmivonwien@gmail.com>
2019-04-01 07:08:27 +0200

    project removed

--- javakurs3-biliothek-jsf-LoginDB/src/main/java/at/bibliothek/model/enums/RoleType.java
+++
@@ -1,6 +0,0 @@
-package at.bibliothek.model.enums;
-
-public enum RoleType {
-
-	 Administrator, general, Moderator;
-}
--- javakurs3-biliothek-jsf-LoginDB/src/main/java/at/bibliothek/services/database/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package at.bibliothek.services.database;
-
-public class UserAlreadyExistsException extends Exception{
-
-	private static final long serialVersionUID = 1L;
-
-}
